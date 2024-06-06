d5909c515550fbcffc0110669954a6270252a52e
qmacias <ezequielqmacias@gmail.com>
2024-06-06 20:51:54 -0300

    Agregar Hero Store (No Implementado Aún)

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore } from 'vuex';
 
 import bookStore from "@/store/books/bookStore";
+import heroStore from "@/store/hero/heroStore";
 import navbarStore from "@/store/navbar/navbarStore";
 import serviceStore from "@/store/services/serviceStore";
 
 export default createStore({
-  modules: { bookStore, navbarStore, serviceStore },
+  modules: { bookStore, heroStore, navbarStore, serviceStore },
 });
